5217773cb2009e54908fa48c78d9bffda41eb32a
Matheus da silva <matheussilvainter1234@gmail.com>
2024-10-24 20:26:22 -0300

    entrega

--- exercicio1.js
+++ exercicio1.js
@@ -1,4 +1,5 @@
 let abrigos = []
+
 function cadastrarAbrigo() {
     let abrigo = {
         nome: prompt("Nome do abrigo:"),
@@ -8,30 +9,35 @@ function cadastrarAbrigo() {
         capacidade : prompt("Capacidade do abrigo:"),
     };
     abrigos.push(abrigo);
- }
+}
  function listarAbrigos() {
     console.log("--------------------\nLISTAGEM DE ABRIGOS:\n--------------------");
-    console.log("CÓDIGO |         NOME         |              ENDEREÇO              |   TELEFONE   |  CAPACIDADE | CIDADE");                       for (let i = 0; i < abrigos.length; i++) {
-        console.log(`${i + 1} | ${abrigos[i].nome} | ${ abrigos[i].endereco} | ${abrigos[i].telefone} | ${ abrigos[i].capacidade} | ${abrigos[i].Cidade}`);
+    console.log("CÓDIGO |         NOME         |              ENDEREÇO              |   TELEFONE   |  CAPACIDADE | CIDADE");
+    console.log("---------------------------------------------------------------------------------------------------------"); 
+    for (let i = 0; i < abrigos.length; i++) {
+        console.log(`${i + 1} | ${abrigos[i].nome} | ${ abrigos[i].endereco} | ${abrigos[i].telefone} | ${ abrigos[i].capacidade} | ${abrigos[i].cidade}`);
     };
     console.log("---------------------------------------------------------------------------------------------------------");
 }
 
 function procurarAbrigo() {
     let cidadeProcurada = prompt ("Qual cidade você está?");
-    let abrigoEncontrado = abrigos.find(abrigo => abrigo.Cidade.toLowerCase() === cidadeProcurada.toLowerCase());
+    let abrigoEncontrado = abrigos.filter(abrigo => abrigo.cidade.toLowerCase() === cidadeProcurada.toLowerCase());
     console.log("--------------------\nLISTAGEM DE ABRIGOS:\n--------------------");
     console.log("CÓDIGO |         NOME         |              ENDEREÇO              |   TELEFONE   |  CAPACIDADE | CIDADE");
-    if (abrigoEncontrado) {
-        console.log(`Abrigo encontrado: ${abrigoEncontrado.nome}  ${abrigoEncontrado.endereco}  ${abrigoEncontrado.telefone}  ${abrigoEncontrado.capacidade}  ${abrigoEncontrado.Cidade}` );
-    } 
-
+    console.log("---------------------------------------------------------------------------------------------------------");
+    if (abrigoEncontrado.length > 0) {
+        for (let i = 0; i < abrigoEncontrado.length;  i++) {
+            console.log(`${i + 1} | ${abrigoEncontrado[i].nome} | ${abrigoEncontrado[i].endereco} | ${abrigoEncontrado[i].telefone} | ${abrigoEncontrado[i].capacidade} | ${abrigoEncontrado[i].cidade}` );
+        };
+        console.log("---------------------------------------------------------------------------------------------------------");
+    }
 }
 
 
 function menu() { 
     let opcao;
-    while (opcao !== '4') {
+    while (true) {
         opcao = prompt("===== ABRIGOS TEMPORÁRIOS ===== \n1. Cadastrar abrigo\n2. Listar abrigos\n3. Procurar abrigo\n4. Sair\nEscolha uma opção:");
         if (opcao === '1') {
             cadastrarAbrigo();
@@ -42,9 +48,7 @@ function menu() {
         } else if (opcao === '4') {
             console.log("Programa encerrado");
             break;
-        } else {
-            console.log("Opção inválida. Tente novamente.\n");
-        }
+        } 
     }
 }
 
